d8e3ca6432e4c6796998a91a9c2194036442e437
kramstyles <mark.eke@decagon.dev>
2022-03-13 05:19:07 +0100

    opened and closed connection

--- models/user.py
+++ models/user.py
@@ -1,2 +1,29 @@
+import psycopg2
+import psycopg2.errors
+
+
 class User:
-    pass
+    def __init__(self):
+        self.conn = self.cursor = None
+
+    def connect(self):
+        self.conn = psycopg2.connect(
+            user='postgres', password='*',
+            host='localhost', port=5432,
+            database='db_decagon'
+        )
+
+        self.cursor = self.conn.cursor()
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+        if self.cursor:
+            self.cursor.close()
+
+    def add(self):
+        print('added')
+
+
+me = User()
+me.add()
