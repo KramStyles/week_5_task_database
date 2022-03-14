807bb4c7a57d3bb4de7927edd0a9404685cfd289
kramstyles <mark.eke@decagon.dev>
2022-03-14 05:50:05 +0100

    working on test for all song

--- models/song.py
+++ models/song.py
@@ -1,2 +1,21 @@
+from datetime import datetime
+
+import psycopg2
+from psycopg2 import Error
+
+
 class Song:
-    pass
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            user='postgres', password='*',
+            port=5432, host='localhost',
+            database='db_decagon'
+        )
+        self.cursor = self.conn.cursor()
+
+    def close_connection(self):
+        if self.conn: self.conn.close()
+        if self.cursor: self.cursor.close()
+
+    def all(self):
+        pass
--- tests/test_song.py
+++ tests/test_song.py
@@ -1,5 +1,13 @@
 import unittest
 
+from psycopg2 import Error
+
+from models import song
+
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.song = song.Song()
+
+    def test_to_select_all_songs_from_a_user(self):
+        pass
