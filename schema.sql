dee55d1833801032d5c514d79430e8be8c9922df
kramstyles <mark.eke@decagon.dev>
2022-03-14 05:41:20 +0100

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,20 +1,23 @@
-CREATE TABLE songs
+DROP TABLE users;
+DROP TABLE songs;
+
+CREATE TABLE users
 (
-    id integer NOT NULL SERIAL,
-    user_id integer NOT NULL,
-    name character varying(100) NOT NULL,
-    genre character varying(50) NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY ,
+    username character varying(50) NOT NULL,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
     created_at date NOT NULL,
     updated_at date,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE users
+CREATE TABLE songs
 (
-    id integer NOT NULL,
-    username character varying(50) NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY ,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ,
+    name character varying(100) NOT NULL,
+    genre character varying(50) NOT NULL,
     created_at date NOT NULL,
     updated_at date,
     PRIMARY KEY (id)
